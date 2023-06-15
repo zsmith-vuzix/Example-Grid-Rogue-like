2e57b5591f08b49c154783bdb0a673acc755bb90
zsmith-vuzix <112656987+zsmith-vuzix@users.noreply.github.com>
2023-06-15 15:11:43 -0400

    Removed event

--- Example-Grid-Rogue-like/Assets/Scripts/GameManager.cs
+++ Example-Grid-Rogue-like/Assets/Scripts/GameManager.cs
@@ -42,8 +42,6 @@ public class GameManager : MonoBehaviour
     public int enemyIncome;
     public Building[] enemybuildings;
 
-    public static event Action<GameState> OnGameStateChanged;
-
 
     private void Awake()
     {
